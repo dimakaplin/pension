91939374b96f0d276a9c28eac3715f27b301bb67
dimakaplin <35988432+dimakaplin@users.noreply.github.com>
2018-09-20 07:20:52 +0300

    all inputs work

--- src/App.js
+++ src/App.js
@@ -20,21 +20,32 @@ class FullResult extends Component {
   }
   calcButtonClick = () => {this.setState({result: !this.state.result})}
   ageInputClick = (event) => {this.setState({age: event.target.value})}
+  sexInputClick = () => {this.setState({sex: this.selectSex.options.selectedIndex})}
   smokingInputClick = () => {this.setState({smoking: this.selectSmoking.options.selectedIndex})}
+  alcoInputClick = () => {this.setState({alco: this.selectAlco.options.selectedIndex})}
   render() {
     return (
+
       <div className = 'central'>
         {this.alertResult()}
         <input id="age" type="number" range = '1' value = {this.state.age} onChange = {this.ageInputClick.bind(this)}></input>
+        <select ref = {(select) => {this.selectSex = select;}} onChange = {this.sexInputClick.bind(this)}>
+          <option value = 'man'>мужчина</option>
+          <option value = 'woman'>женщина</option>
+        </select> 
         <select ref = {(select) => {this.selectSmoking = select;}} onChange = {this.smokingInputClick.bind(this)}>
           <option value = 'yes'>да</option>
           <option value = 'no'>нет</option>
-        </select>  
+        </select>
+        <select ref = {(select) => {this.selectAlco = select;}} onChange = {this.alcoInputClick.bind(this)}>
+          <option value = 'yes'>да</option>
+          <option value = 'no'>нет</option>
+        </select>   
         <button onClick = {this.calcButtonClick}>жми</button>
       </div>
-    );
+    )};
   }
-}
+
 
 class App extends Component {
   render() {
